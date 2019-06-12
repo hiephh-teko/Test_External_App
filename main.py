3eb35e967943af263de3975f30e4351d8eb5f9e8
hiephh-teko <hiep.hh@teko.vn>
2019-06-12 18:05:07 +0700

    update job

--- main.py
+++ main.py
@@ -2,16 +2,31 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
+import sqlalchemy as db
+
 
 _DOT_ENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(_DOT_ENV_PATH)
 
 engine = create_engine(os.getenv('DATABASE_URI'))
 
-sql = text('SELECT 1')
+connection = engine.connect()
+metadata = db.MetaData()
+goals = db.Table('goals', metadata, autoload=True, autoload_with=engine)
 
-result = engine.execute(sql)
+query = db.select([goals])
+ResultPoroxy = connection.execute(query)
+Res = ResultPoroxy.fetchall()
+print(Res)
 
-data = [row[0] for row in result]
+# print(goals.columns.keys())
+# print(repr(metadata.tables['goals']))
 
-print(data)
+
+# sql = text('SELECT 1')
+
+# result = engine.execute(sql)
+
+# data = [row[0] for row in result]
+
+# print(data)
