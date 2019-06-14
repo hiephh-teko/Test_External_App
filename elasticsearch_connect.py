aac2b6b2435edbe954aa820ef6622f9840020712
hiephh-teko <hiep.hh@teko.vn>
2019-06-14 16:49:31 +0700

    update regex query

--- elasticsearch_connect.py
+++ elasticsearch_connect.py
@@ -7,47 +7,61 @@ class ElasticsearchConnect(object):
     def __init__(self, data): 
         self.data = data
 
-    def open_connect(self):
-        res = requests.get('http://103.126.156.112:9200')
-        print(res.status_code)
-        es = Elasticsearch([{'host': '103.126.156.112', 'port': 9200}])
-        
-        eventname_text = f"event.{str(self.data[0].match_attribute)}".rstrip()
-        pattern_text = str(self.data[0].pattern)
+    def query_contain_type(self, es, field, value):
+        print(field,value)
         body = {
             "size": 0, 
             "query":{
                 "constant_score" : { 
                     "filter" : {
                         "term" : { 
-                            eventname_text : pattern_text
+                            field : value
                         }
                     }
                 }
-            }     
-            
+            }      
         }
-        
         res2 = es.search(index='tracking-chat-tool-v2-*',body=body)
-        print(res2)
+        # print(res2)
         print(res2['hits']['total'])
+    
+    def query_regex_type(self, es, field, value):
+        print(field,value)
+        body = {
+            "size": 0, 
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "regexp": {
+                                field:{
+                                    "value": value
+                                }
+                            }
+                        }
+                    ]
+                }
+            }   
+        }
+        print(body)
+        res2 = es.search(index='tracking-chat-tool-v2-*',body=body)
+        # print(res2)
+        # print(res2['hits']['total'])
+
+    def open_connect(self):
+        es = Elasticsearch([{'host': '103.126.156.112', 'port': 9200}])
+
+        for element_data in self.data:
+
+            field = str(f"event.{str(element_data.match_attribute)}")
+            value = str(element_data.pattern)
+
+            if str(element_data.pattern_type) == "contains":
+                self.query_contain_type(es,field,value)
+            elif str(element_data.pattern_type) == "regex":
+                # print(field,value)
+                self.query_regex_type(es,type,field)
+            
+
+            
         
-
-
-# #connect to our cluster
-# from elasticsearch import Elasticsearch
-# es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
-
-#let's iterate over swapi people documents and index them
-
-# r = requests.get('http://localhost:9200') 
-# i = 1
-# while r.status_code == 200:
-#     r = requests.get('http://swapi.co/api/people/'+ str(i))
-#     es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content))
-#     i=i+1
- 
-# print(i)
-
-# print(es.get(index='sw', doc_type='people', id=5))
